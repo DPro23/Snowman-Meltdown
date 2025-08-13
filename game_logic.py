b13017d7b779521f9646d17d70fbe34048d886ac
DaNiLo <danilo.p2391@gmail.com>
2025-08-13 11:31:32 +0200

    Style CLI, create function for victory checks, ask to restart game

--- game_logic.py
+++ game_logic.py
@@ -2,7 +2,7 @@ import random
 from ascii_art import STAGES
 
 # List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
+WORDS = ["python", "git", "github", "snowman", "meltdown", "master", "javascript", "dev"]
 
 
 def get_random_word():
@@ -11,7 +11,7 @@ def get_random_word():
 
 
 def display_game_state(mistakes, secret_word, guessed_letters):
-    # Display the snowman stage for the current number of mistakes.
+    """Displays the snowman stage for the current number of mistakes"""
     print(STAGES[mistakes])
     # Build a display version of the secret word.
     display_word = ""
@@ -20,25 +20,38 @@ def display_game_state(mistakes, secret_word, guessed_letters):
             display_word += letter + " "
         else:
             display_word += "_ "
-    print("Word: ", display_word)
-    print("\n")
+    print("💬Word: ", display_word, "\n")
+
+def is_word_guessed(secret_word, letters_guessed):
+    """Return True if all the letters in secret_word are in letters_guessed."""
+    for letter in secret_word:
+        if letter not in letters_guessed:
+            return False
+    return True
 
 
 def play_game():
+    """Run the game loop"""
+    mistakes_limit = 3
     secret_word = get_random_word()
     guessed_letters = []
     mistakes = 0
+    playing = True
+    end_game = False
 
-    # For now, display the initial game state.
-    print("Welcome to Snowman Meltdown!")
-    # Prompt user for one guess (logic to be enhanced later)
-    while True:
+    # Print a pretty Game title.
+    print('*' * 33)
+    print("\u001b[1m⛄️Welcome to Snowman Meltdown!⛄️")
+    print('*' * 33)
+
+    # Ask user to guess a letter of secret_word
+    while playing:
         display_game_state(mistakes, secret_word, guessed_letters)
-        guess = input("Guess a letter: ").lower()
+        guess = input("🤔Guess a letter: ").lower()
         print("You guessed:", guess)
 
         if len(guess) != 1:
-            print("Please enter a single letter.")
+            print("‼️Please enter a single letter.")
             continue
 
         if guess in secret_word:
@@ -46,11 +59,30 @@ def play_game():
         else:
             mistakes += 1
 
-        if mistakes == 3:
-            print("The snowman didn't survive! The secret word was:", secret_word)
+        # Game over
+        if mistakes == mistakes_limit:
             display_game_state(mistakes, secret_word, guessed_letters)
-            break
+            end_game = True
+            print("The snowman didn't survive! 🫠\nThe secret word was:", secret_word)
 
-        if len(guessed_letters) == len(secret_word):
-            print("Congratulations, you saved the Snowman!!!")
-            break
+        # Victory
+        if is_word_guessed(secret_word, guessed_letters):
+            display_game_state(mistakes, secret_word, guessed_letters)
+            end_game = True
+            print("🏆Congratulations, you saved the Snowman!!! ⛄️🥳")
+
+        # User can restart after the word is completed or mistakes == mistakes_limit
+        while end_game:
+            restart = input("🔄Would you like to play again? (y/n): ").lower()
+
+            if restart == "y":
+                play_game()
+
+            elif restart == "n":
+                playing = False
+                end_game = False
+                print("Thank you for playing! 👋")
+
+            else:
+                print('‼️Please enter only "y" or "n" as input.')
+                continue
